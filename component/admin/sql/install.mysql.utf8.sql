c9ae50d3f793deb94d0f745c4b4d26554958dc6a
Alexander Krantz <klako@hotmail.se>
2020-07-05 01:40:27 +0200

    Added new tables for storing local roles etc.

--- component/admin/sql/install.mysql.utf8.sql
+++ component/admin/sql/install.mysql.utf8.sql
@@ -1,20 +1,114 @@
-DROP TABLE IF EXISTS `#__scoutorg_troops`;
 DROP TABLE IF EXISTS `#__scoutorg_branches`;
-
 CREATE TABLE `#__scoutorg_branches` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_customlistmembers`;
+CREATE TABLE `#__scoutorg_customlistmembers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customlist_source` varchar(45) NOT NULL,
+  `customlist_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_customlists`;
+CREATE TABLE `#__scoutorg_customlists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `description` text,
+  `parent_source` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_groupmemberroles`;
+CREATE TABLE `#__scoutorg_groupmemberroles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `role_source` varchar(45) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_grouproles`;
+CREATE TABLE `#__scoutorg_grouproles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `#__scoutorg_patrolmemberroles`;
+CREATE TABLE `#__scoutorg_patrolmemberroles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patrolmember_source` varchar(45) NOT NULL,
+  `patrolmember_id` int(11) NOT NULL,
+  `role_source` varchar(45) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_patrolmembers`;
+CREATE TABLE `#__scoutorg_patrolmembers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patrol_source` varchar(45) NOT NULL,
+  `patrol_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_patrolroles`;
+CREATE TABLE `#__scoutorg_patrolroles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_patrols`;
+CREATE TABLE `#__scoutorg_patrols` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `troop_source` varchar(45) NOT NULL,
+  `troop_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_troopmemberroles`;
+CREATE TABLE `#__scoutorg_troopmemberroles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `troopmember_source` varchar(45) NOT NULL,
+  `troopmember_id` int(11) NOT NULL,
+  `role_source` varchar(45) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_troopmembers`;
+CREATE TABLE `#__scoutorg_troopmembers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `troop_source` varchar(45) NOT NULL,
+  `troop_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_trooproles`;
+CREATE TABLE `#__scoutorg_trooproles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `#__scoutorg_troops`;
 CREATE TABLE `#__scoutorg_troops` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `troop` int(11) unsigned NOT NULL,
-  `branch` int(11) unsigned NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `branch` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `#__scoutorg_troops_branch_idx` (`branch`),
-  CONSTRAINT `#__scoutorg_troops_branch` FOREIGN KEY (`branch`) REFERENCES `#__scoutorg_branches` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `#__scoutorg_branches` (`name`)
 VALUES ('Spårare'), ('Upptäckare'), ('Äventyrare'), ('Utmanare');
