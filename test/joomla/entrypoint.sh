2af996fa5ed47dbf88fe8f3bd42db52d8f66ca31
Alexander Krantz <klako@hotmail.se>
2020-07-04 17:57:55 +0200

    Fixed install and code linking.

--- test/joomla/entrypoint.sh
+++ test/joomla/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-joomla site:install testsite --options=configuration.yaml \
+sleep 10
+
+joomla site:install testsite --options=configuration.yaml -vvv \
     --mysql-login=${JOOMLA_MYSQL_LOGIN} \
     --mysql-host=${JOOMLA_MYSQL_HOST} \
     --mysql-database=${JOOMLA_MYSQL_DATABASE}
@@ -9,8 +11,8 @@ joomla vhost:create testsite --disable-ssl --http-port=81
 joomla extension:installfile testsite /root/component
 rm -r /var/www/testsite/components/com_scoutorg
 rm -r /var/www/testsite/administrator/components/com_scoutorg
-rm /var/www/testsite/language/{sv-SE,en-GB}/{sv-SE,en-GB}.com_scoutorg.{sys.ini,ini}
-rm /var/www/testsite/administrator/language/{sv-SE,en-GB}/{sv-SE,en-GB}.com_scoutorg.{sys.ini,ini}
+rm /var/www/testsite/language/{sv-SE,en-GB}/{sv-SE,en-GB}.com_scoutorg.{sys.ini,ini} 2> /dev/null
+rm /var/www/testsite/administrator/language/{sv-SE,en-GB}/{sv-SE,en-GB}.com_scoutorg.{sys.ini,ini} 2> /dev/null
 joomla extension:symlink testsite com_scoutorg --projects-dir /var/www/projects
 
 /usr/sbin/apache2ctl -D FOREGROUND
