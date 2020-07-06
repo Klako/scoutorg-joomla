a83e100960e9805d786e1fa983affe00ae039125
Alexander Krantz <klako@hotmail.se>
2020-07-07 00:50:10 +0200

    Dont show table when empty.

--- component/admin/views/branches/tmpl/default.php
+++ component/admin/views/branches/tmpl/default.php
@@ -11,43 +11,45 @@ HTMLHelper::_('formbehavior.chosen', 'select');
 ?>
 <form action="index.php?option=com_scoutorg&view=branches" method="post" id="adminForm" name="adminForm">
 	<div id="j-sidebar-container" class="span2">
-    	<?= $this->sidebar; ?>
+		<?= $this->sidebar; ?>
 	</div>
 	<div id="j-main-container" class="span10 j-toggle-main">
-		<table class="table table-striped table-hover">
-			<thead>
-			<tr>
-				<th width="1%"><?= Text::_('COM_SCOUTORG_NUM') ?></th>
-				<th width="2%">
-					<?= HTMLHelper::_('grid.checkall'); ?>
-				</th>
-				<th width="90%">
-					<?= Text::_('COM_SCOUTORG_BRANCH_NAME_LABEL') ?>
-				</th>
-				<th width="2%">
-					<?= Text::_('COM_SCOUTORG_BRANCH_ID_LABEL') ?>
-				</th>
-			</tr>
-			</thead>
-			<tfoot>
-				<tr>
-					<td colspan="5">
-						<?= $this->pagination->getListFooter() ?>
-					</td>
-				</tr>
-			</tfoot>
-			<tbody>
-				<?php if (!empty($this->items)) : ?>
-					<?php foreach ($this->items as $i => $row) :
-						$link = Route::_('index.php?option=com_scoutorg&task=branch.edit&id=' . $row->id);
-					?>
+		<?php if (empty($this->items)) : ?>
+			<div class="alert alert-no-items">
+				<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+			</div>
+		<?php else : ?>
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th width="1%"><?= Text::_('COM_SCOUTORG_NUM') ?></th>
+						<th width="2%">
+							<?= HTMLHelper::_('grid.checkall'); ?>
+						</th>
+						<th width="90%">
+							<?= Text::_('COM_SCOUTORG_BRANCH_NAME_LABEL') ?>
+						</th>
+						<th width="2%">
+							<?= Text::_('COM_SCOUTORG_BRANCH_ID_LABEL') ?>
+						</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td colspan="5">
+							<?= $this->pagination->getListFooter() ?>
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+					<?php foreach ($this->items as $i => $row) : ?>
 						<tr>
 							<td><?= $this->pagination->getRowOffset($i); ?></td>
 							<td>
 								<?= HTMLHelper::_('grid.id', $i, $row->id); ?>
 							</td>
 							<td>
-								<a href="<?= $link; ?>" title="<?= Text::_('COM_SCOUTORG_EDIT_BRANCH'); ?>">
+								<a href="<?= Route::_('index.php?option=com_scoutorg&task=branch.edit&id=' . $row->id) ?>" title="<?= Text::_('COM_SCOUTORG_EDIT_BRANCH'); ?>">
 									<?= $row->name; ?>
 								</a>
 							</td>
@@ -56,12 +58,11 @@ HTMLHelper::_('formbehavior.chosen', 'select');
 							</td>
 						</tr>
 					<?php endforeach; ?>
-				<?php endif; ?>
-			</tbody>
-		</table>
-		<input type="hidden" name="task" value=""/>
-		<input type="hidden" name="boxchecked" value="0"/>
+				</tbody>
+			</table>
+		<?php endif; ?>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
 		<?= HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
-
