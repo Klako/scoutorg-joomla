df929864bd891578e491b1002d9eeef33059dfc9
Alexander Krantz <klako@hotmail.se>
2020-07-17 00:39:56 +0200

    Created custom controller.

--- component/admin/controllers/troop.php
+++ component/admin/controllers/troop.php
@@ -1,7 +1,22 @@
 <?php
 
-use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Router\Route;
 
-class ScoutOrgControllerTroop extends FormController {
-    
+class ScoutOrgControllerTroop extends BaseController
+{
+    public function add()
+    {
+        $this->setRedirect(Route::_('index.php?option=com_scoutorg&view=troop'));
+    }
+
+    public function edit()
+    {
+        Factory::getApplication()->allowCache(false);
+
+        $id = Factory::getApplication()->input->getString('id');
+
+        $this->setRedirect(Route::_('index.php?option=com_scoutorg&view=troop&id=' . $id, false));
+    }
 }
