b73b09640b2c24b8b8c1087f905b18d56c094a2f
Klako <klako@hotmail.se>
2020-07-20 03:21:06 +0200

    Added branchtroop field in branch editor.

--- component/admin/models/branch.php
+++ component/admin/models/branch.php
@@ -36,6 +36,10 @@ class ScoutorgModelBranch extends OrgObjectModel
         if ($branch) {
             $data['uid'] = $branch->uid->serialize();
             $data['name'] = $branch->name;
+            $data['troops'] = [];
+            foreach ($branch->troops as $troop) {
+                $data['troops'][] = $troop->uid->serialize();
+            }
         }
         return $data;
     }
@@ -45,6 +49,8 @@ class ScoutorgModelBranch extends OrgObjectModel
         jimport('scoutorg.loader');
         /** @var ScoutOrgTableBranch|CMSObject */
         $branchTable = $this->getTable('Branch');
+        /** @var ScoutOrgTableBranchtroop|CMSObject */
+        $branchtroopTable = $this->getTable('Branchtroop');
 
         $uid = Uid::deserialize($data['uid']);
 
@@ -65,6 +71,47 @@ class ScoutorgModelBranch extends OrgObjectModel
         }
 
         $uid = new Uid('joomla', $branchTable->id);
+
+        jimport('scoutorg.loader');
+        $scoutorg = ScoutorgLoader::load();
+        $branch = $scoutorg->branches->get($uid);
+        $troopsToAdd = [];
+        foreach ($data['troops'] as $troop) {
+            $troopsToAdd[$troop] = $troop;
+        }
+        $troopsToRemove = [];
+
+        foreach ($branch->troops as $troop) {
+            $troopUid = $troop->uid->serialize();
+            if (!$troopsToAdd[$troopUid]) {
+                $troopsToRemove[] = $troopUid;
+            } else {
+                unset($troopsToAdd[$troopUid]);
+            }
+        }
+
+        foreach ($troopsToAdd as $troopUid) {
+            $branchtroopTable->id = null;
+
+            $branchtroopData = [
+                'branch' => $uid->serialize(),
+                'troop' => $troopUid
+            ];
+
+            if (!$branchtroopTable->save($branchtroopData)) {
+                /** @var CMSObject $branchTroopTable */
+                /** @var CMSObject $this */
+                $this->setError('unable to save branchtroop' . $branchtroopTable->getError());
+
+                return false;
+            }
+        }
+
+        foreach ($troopsToRemove as $troopUid) {
+            $branchtroopTable->load(['troop' => $troopUid]);
+            $branchtroopTable->delete();
+        }
+
 
         $this->setState('branch.id', $uid->serialize());
 
