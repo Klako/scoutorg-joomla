a9d6f03de950f5041895519b6291c04423b587f7
Alexander Krantz <klako@hotmail.se>
2020-08-05 01:44:42 +0200

    Updated quicksearch and added to custom subform

--- component/admin/models/fields/orgobjectsubform.php
+++ component/admin/models/fields/orgobjectsubform.php
@@ -1,7 +1,10 @@
 <?php
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
 
 defined('_JEXEC') or die('Restricted access');
 
@@ -17,8 +20,58 @@ class JFormFieldOrgobjectsubform extends JFormFieldSubform
 
     protected function getInput()
     {
+        /** @var \Joomla\CMS\Document\Document $document */
+        $document = Factory::getDocument();
+        $document->addScript(Uri::root() . 'media/com_scoutorg/js/jquery.quicksearch.js');
+
         /** @var JFormFieldOrgobjectsubform|FormField $this */
         $this->buttons = $this->form->subformbuttons;
-        return parent::getInput();
+
+        $html = [];
+
+        $filterText = Text::_('Filter');
+
+        $html[] = <<<HTML
+        <input type="text" id="{$this->fieldname}-search" placeholder="{$filterText}" />
+        <span id="{$this->fieldname}-search-label">test</span>
+        HTML;
+
+        $html[] = parent::getInput();
+
+        $html[] = <<<HTML
+        <script>
+            (function(){
+                let searchBox = jQuery("#patrols-search");
+                let resultBox = jQuery('#{$this->fieldname}-search-label');
+                let search = jQuery("#{$this->fieldname}-search").quicksearch('#patrols-search ~ div table > tbody > tr', {
+                    onBefore: () => {
+                        search.cache();
+                    },
+                    testQuery: (query, txt, row) => {
+                        for (var i = 0; i < query.length; i += 1) {
+                            var foundInValue = false;
+                            var inputs = jQuery(row).find('input');
+                            for (var j = 0; j < inputs.length; j += 1) {
+                                if (jQuery(inputs[j]).is(':hidden')){
+                                    continue;
+                                }
+                                if (inputs[j].value.toLowerCase().indexOf(query[i]) !== -1) {
+                                    foundInValue = true;
+                                    break;
+                                }
+                            }
+                            if (!(txt.indexOf(query[i]) !== -1 || foundInValue)) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                });
+                search.cache();
+            })();
+        </script>
+        HTML;
+
+        return implode('', $html);
     }
 }
